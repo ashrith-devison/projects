272eab2e484c883d9a2d83c7ffced7d1bfa36bc4
ashrith-devison <158307747+ashrith-devison@users.noreply.github.com>
2024-02-20 11:07:13 +0530

    Update speech.js

--- speech-recognition/speech.js
+++ speech-recognition/speech.js
@@ -1,1 +1,20 @@
+click_to_convert.addEventListener('click', function() {
 
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.interiemResults = true;
+
+    recognition.addEventListener('result', e=> {
+        const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        convert_text.innerHTML = transcript;
+        console.log(transcript);
+
+    })
+    if(speech === true){
+        recognition.start();
+    }
+}
+)
